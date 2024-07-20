bd02a5ce4047ce3890bf42753df489a85f244a23
MarkoPoloMA <mark.utrobin@mail.ru>
2024-07-20 14:30:33 +0400

    Task1 fixed

---
+++ Task1A/Task1.CSharpBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1A
+{
+    class Class1
+    {
+        int age;
+        string name;
+        string comporationName;
+        bool IsCompare;
+        double bodyWeights;
+    }
+}
