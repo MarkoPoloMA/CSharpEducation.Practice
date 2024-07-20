df4912373a31a94e0799fad558e4de5a2d14ec8e
MarkoPoloMA <mark.utrobin@mail.ru>
2024-07-20 14:59:11 +0400

    Task3

---
+++ Task1A/Task3.CSharpBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Task1A
+{
+    class ClassTask3
+    {
+       public static void Main()
+        {
+            Console.WriteLine("Введите ваше имя: ");
+            string myName = Console.ReadLine();
+
+            Console.WriteLine($"Ваше имя - {myName}. Привет {myName}");
+
+            Console.ReadKey();
+        }
+    }
+}
